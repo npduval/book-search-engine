fbe9a2d5416a5cc63541ad4bac871aad34d0acf5
Nick Duval <nicholaspduval74@gmail.com>
2022-02-25 15:45:57 -0500

    updated server.js to include appolo server as middelware

--- server/server.js
+++ server/server.js
@@ -6,16 +6,18 @@ const { ApolloServer } = require('apollo-server-express');
 
 
 
-const { typeDefs, resolvers } = require('./schemas');
+// const { typeDefs, resolvers } = require('./schemas');
 
 
-
-
-
-
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
+server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -24,7 +26,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`localhost:${PORT}`));
