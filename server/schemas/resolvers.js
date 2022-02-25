f3b461d4e5f07583c12d29133aa863910804d4d2
Nick Duval <nicholaspduval74@gmail.com>
2022-02-25 17:33:25 -0500

    started resolvers and typeDefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+// import user model
+const { User } = require('../models');
+// import sign token function from auth
+const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+    query: {
+        
+    }
+}
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  
+  type Book {
+      bookID: String
+      authors: [String]
+      description: String
+      title: String
+      image: String
+      link: String
+  }
+  
+  type Auth {
+      token: ID!
+      user: User
+  }`
